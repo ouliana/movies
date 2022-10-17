589b0de4c3d368c14c8ea5274f5f162b0c5ca293
ouliana <ouliana.kotik@gmail.com>
2022-10-17 11:29:10 +0300

    Add request

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Movies from '../components/Movies'
 
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
     constructor() {
         super();
@@ -10,9 +13,19 @@ class Main extends React.Component {
         }
     }
 
+    componentDidMount() {
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=star trek`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => this.setState({ movies: data.Search, loading: false }));
+    }
+
     render() {
+        const { movies, loading } = this.state;
+        
         return <main>
-           <Movies />
+           <Movies movies={movies} /> 
         </main>
     }
 

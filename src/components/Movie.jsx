5acb0ad8568bf3a01f1fc0f9a5d818861a8578c1
ouliana <ouliana.kotik@gmail.com>
2022-10-17 11:15:11 +0300

    Add code for Movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,7 +1,25 @@
 function Movie(props) {
-    return <div>
-        +++++
-    </div>
+    const {
+        imdbID, 
+        Poster: poster, 
+        Title: title, 
+        Type: type, 
+        Year: year
+    } = props;
+    
+    return <div id={imdbID} className="card">
+                <div className="card-image waves-effect waves-block waves-light">
+                {
+                    poster === 'N/A' ? <img className="activator" src={`https://via.placeholder.com/300x450?text=${title}`} alt=""/>
+                    : <img className="activator" src={poster} alt=""/>
+                }
+                
+                </div>
+                <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{title}</span>
+                <p>{type} <span className="right">{year}</span></p>
+                </div>
+        </div>
 }
 
 export default Movie

824e92f3f945f61afc95e2881d7946b128c6c5bc
ouliana <ouliana.kotik@gmail.com>
2022-10-17 11:12:08 +0300

    Add Movie and Movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,15 @@
 import Movie from './Movie'
 
 function Movies(props) {
-    return <div>
-        <Movie />
-    </div>
+    const { movies } = props;
+    
+    return <div className='movies'>
+        {
+            movies ? movies.map(movie => (
+                    <Movie key={movie.imdbID} {...movie} />
+                )) : <h4>Movie not found!</h4>
+             }
+         </div>
 }
 
 export default Movies
